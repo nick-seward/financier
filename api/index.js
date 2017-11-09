525fac7ccd5f876a712bf0237c5b18868bd0d049
Caleb Tucker <calebt@gmail.com>
2017-11-09 16:39:09 -0600

    added /db proxy to local couchdb

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 var path = require('path');
 var express = require('express');
+var proxy = require('express-http-proxy');
 var app = express();
 
 app.use('/docs', express.static(path.join(__dirname, '../docs')));
 
 app.use(express.static(path.join(__dirname, '../dist')));
+
+app.use('/db', proxy('127.0.0.1:5984'));
 
 // html5mode
 app.all('/*', function(req, res) {
@@ -12,5 +15,5 @@ app.all('/*', function(req, res) {
 });
 
 app.listen(8080, function () {
-  console.log('Financier frontend listening on port 8080!');
+  console.log('Financier frontend listening on port 8080!!!');
 });
