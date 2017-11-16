8dfa9ca490642b144d038ac2a91172eaa51574cb
Caleb Tucker <calebt@gmail.com>
2017-11-15 20:52:51 -0600

    updating script

--- install/ubuntu_install.sh
+++ install/ubuntu_install.sh
@@ -4,6 +4,7 @@
 
 # Install couchdb
 echo "deb https://apache.bintray.com/couchdb-deb xenial main" | sudo tee -a /etc/apt/sources.list
+curl -L https://couchdb.apache.org/repo/bintray-pubkey.asc | sudo apt-key add -
 sudo apt-get update && sudo apt-get install -y couchdb
 
 # Install node
